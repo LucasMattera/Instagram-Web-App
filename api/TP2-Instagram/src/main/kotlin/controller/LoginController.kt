7115e729328c2b42ea153ae67e21b7bc7e364a79
Lucas Mattera <lucasmattera97@gmail.com>
2020-09-29 02:05:49 -0300

    add createLogin func

--- api/TP2-Instagram/src/main/kotlin/controller/LoginController.kt
+++ api/TP2-Instagram/src/main/kotlin/controller/LoginController.kt
@@ -1,4 +1,11 @@
 package controller
 
-class LoginController {
+import io.javalin.http.Context
+import org.unq.ui.model.InstagramSystem
+
+class LoginController(private val instagramSystem : InstagramSystem) {
+
+    fun createLogin(ctx: Context, email: String, psw: String) {
+        ctx.json(instagramSystem.login(email, psw))
+    }
 }
