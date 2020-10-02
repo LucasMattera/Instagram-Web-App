78b6a71ba1d3d068fceaf0db44dcbb7011755c0b
Lucas Mattera <lucasmattera97@gmail.com>
2020-10-01 23:25:42 -0300

    add dependencias JWT

--- api/TP2-Instagram/src/main/kotlin/controller/LoginController.kt
+++ api/TP2-Instagram/src/main/kotlin/controller/LoginController.kt
@@ -4,12 +4,12 @@ import io.javalin.http.Context
 import org.unq.ui.model.InstagramSystem
 import org.unq.ui.model.User
 
-data class UserLogin o Adapter(val email: String, val password: String )
+data class UserLogin(val email: String, val password: String )
 
 class LoginController(private val instagramSystem : InstagramSystem) {
 
     fun loginUser(ctx: Context) {
-        val usr = ctx.body<User>()  //aca se matchea el dataclass con el json que se escribe en postman
+        val usr = ctx.body<UserLogin>()  //aca se matchea el dataclass con el json que se escribe en postman
         ctx.json(instagramSystem.login(usr.email, usr.password))
     }
 
