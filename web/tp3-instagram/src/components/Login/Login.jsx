29f5a5bc2e493fe9f44c0709311bd00fe697be90
nachochelasco <nchelasco@gmail.com>
2020-11-05 23:31:23 -0300

    Diseño de login hecho

--- web/tp3-instagram/src/components/Login/Login.jsx
+++ web/tp3-instagram/src/components/Login/Login.jsx
@@ -1,14 +1,46 @@
 import React, { useState } from "react";
+import '../../styles/Login.css';
+import logo from '../../images/fotoInstagram.png';
+import logoig from '../../images/instagram-new-logo.png' ;
 
 const Login = () => {
     const [email,setEmail] = useState("") ;
+    const [password,setPassword] = useState("") ;
+
 
     return (
-        <div className="input">
-            <input type="text" id="email" name="email" placeholder="Email" required/>
-            <input type="text" id="password" name="password" placeholder="Password" required/>
-            <button type="button">Sign in</button>
+        <div className="container-fluid">
+
+            <div className="row">
+                <div id="columnaIzquierda" className="col-md-3">
+                    <div id="fotoInstagram">
+                        <div className="foto">
+                        <img src={logo} className="celu"/>
+                        </div> 
+                    </div>
+                </div>
+                
+                <div id="columnaDerecha" className="col-md-3 text-center">            
+                    <img src={logoig} className="fontIg"/>
+                        <form>
+                            <div className="form-group" >
+                                <input class="form-control" type="text" name="email" placeholder="Email" required />
+                            </div>
+                            <div className="form-group" >
+                                <input class="form-control" type="password" name="password" placeholder="Password" required />
+                            </div>
+                            <div className="button">
+                                <button type="buttonLogin" class="btn btn-primary btn-lg btn-block">Iniciar sesion</button>
+                            </div>
+                            <div id="register">
+                            <h6>No tienes cuenta ?</h6>
+                                <button type="buttonRegister" class="btn btn-link">Registrate</button>
+                            </div> 
+                        </form>   
+                </div>
+            </div>
         </div>
+                            
     )
 }
 
