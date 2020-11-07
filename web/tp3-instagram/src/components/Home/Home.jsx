b68d62c8cd4b16b929708b82f99913ecf0151774
nachochelasco <nchelasco@gmail.com>
2020-11-07 13:12:21 -0300

    Navbar hecho

--- web/tp3-instagram/src/components/Home/Home.jsx
+++ web/tp3-instagram/src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from '../Navbar/Navbar'
 
 const Home = () => {
+
     return (
         <div className="home">
-            <h1>Home</h1>
+            <Navbar />
         </div>
     )
 }
