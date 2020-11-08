ef0461df3b1228157fdad8c67d8c31667c0c4c36
Lucas Mattera <lucasmattera97@gmail.com>
2020-11-08 02:40:06 -0300

    add Register API connection, no test

--- web/tp3-instagram/src/components/Register/Register.jsx
+++ web/tp3-instagram/src/components/Register/Register.jsx
@@ -1,4 +1,42 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import Notifications,{notify} from 'react-notify-toast';
+import axios from "axios";
+
 const Register = () => {
+    const history = useHistory();
+    const [data, setData] = useState({
+        username: "",
+        email: "",
+        password: "",
+        passwordCheck: ""
+    });
+
+    let myColor = { background: '#0E1717', text: "#FFFFFF" };
+
+    const handleInputChange = (event) => {
+        setData({
+            ...data,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post("http://localhost:7000/register", data)
+            .then((response) => {    
+                localStorage.setItem("token", response.headers.authorization);
+                axios.defaults.headers['authorization'] = localStorage.getItem('token')
+                history.push("/");       
+            })
+            .catch(error => {
+                console.log("error : ", error.response.data.message);
+                const errorUser = error.response.data.message ;
+                notify.show(errorUser,"error",5000,myColor);          
+            });
+        
+    };
+ 
     return (
         <div className="input">
             <input type="text" id="username" name="username" placeholder="Username" required/>
