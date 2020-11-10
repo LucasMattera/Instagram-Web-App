4f66987646b54b27b7769baafbcde3d64a9e06a5
nachochelasco <nchelasco@gmail.com>
2020-11-09 23:32:07 -0300

    Visualizacion de followers en home sin css

--- web/tp3-instagram/src/components/Home/Home.jsx
+++ web/tp3-instagram/src/components/Home/Home.jsx
@@ -3,36 +3,45 @@ import Navbar from '../Navbar/Navbar'
 import axios from "axios";
 
 const Home = () => {
-    const [timeline, setTimeline] = useState([]) ;
+    const [user,setUser] = useState({
+        name: "",
+        image: "",
+        followers: [],
+        timeline: [],
+    })
 
     useEffect(() => {
-        const getUserTimeline = async () =>{
             axios.get("http://localhost:7000/user")
-            .then(response =>{
-                setTimeline(response.data.timeline)
+            .then(success =>{               
+                setUser(success.data)
+                
             })
-        }
-        getUserTimeline();
+            .catch(error =>
+                console.log(error)
+            )            
     }, []
-    )
+    );
+       
+    console.log(user)
 
-    console.log(timeline)
-   
-    return (
+    return(
         <div className="home">
             <Navbar />
-            <div className="contenedor-posts">
-                    {timeline.map(post =>  (
-                        <div className="container">
-                            <div className="posts">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="posts col-md-3">
+                    </div>
+                    <div className="posts col-md-5">
+                        {user.timeline.map(post =>  (
+                            <div className="post">
                                 <div className="imageUserPost">
-                                   <img src={post.user.image}/>
+                                    <img src={post.user.image}/>
                                 </div>
                                 <div className="nameUserPost">
                                     <p>{post.user.name}</p>
                                 </div>
                                 <div className="imageUserPost">
-                                    <img src={post.landscape}/>
+                                    <img src={post.portrait}/>
                                 </div>
                                 <div className="likeUserPost">
                                     <p>{post.likes.length}</p>
@@ -41,12 +50,33 @@ const Home = () => {
                                     <p>{post.description}</p>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>      
+                    <div className="posts col-md-4">
+                            <div className="userName">
+                                <p>{user.name}</p>
+                            </div>
+                            <div className="imagenUserName">
+                                <img src={user.image}/>
+                            </div>
+                            <div className="followers">
+                                <h5>Followers</h5>
+                                {user.followers.map(follower => (
+                                    <ul>
+                                        <img src={follower.image}/>
+                                        <p>{follower.name}</p>
+                                    </ul>
+                                ))}
+                            </div>
+                    </div>
+                </div>
             </div>
         </div>
+        
     )
 }
 
 
 export default Home ;
+        /*
+            */
